68eba6d3e9872c38bfb638de81bf5effe05c81de
Akash <akashpa93@gmail.com>
2015-01-04 02:40:59 +0530

    New York Times AJAX request

--- js/script.js
+++ js/script.js
@@ -12,18 +12,30 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-
-    // YOUR CODE GOES HERE!
     
     var $street = $('#street').val();
     var $city = $('#city').val();
-    $greeting.text("So, you want to live at " + $street + ", " + $city + "?")
+    $greeting.text("So, you want to live at " + $street + ", " + $city + "?");
 
     var $source = "https://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + $street + ", " + $city ;
     $body.append('<img class="bgimg" src="' + $source + '">');
+
+    //New York Times AJAX request
+
+    var $url ="http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+$street+", "+$city+"&sort=newest&api-key=cffedb1973e370b99ba31c25d1e44a8c:18:70667414";
+    
+    $nytHeaderElem .text("New York Times Articles about " + $street + ", " + $city);
+
+    $.getJSON($url,function(data){
+       $.each(data.response.docs,function(key, value) {
+            console.log(value);
+            $nytElem.append('<li class="articles"></li>');
+            $('.articles:last').append('<a href="'+ value.web_url +'">'+ value.snippet +'</a>');
+            $('.articles:last').append('<p>'+ value.lead_paragraph +'</p>');         
+       })    
+    });
     return false;
 };
 
 $('#form-container').submit(loadData);
 
-// loadData();
